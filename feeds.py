e3eabca4355df54b730cebaedeacc42ab9695a55
bharatbuzzai <bharatbuzzai@gmail.com>
2025-09-20 12:44:28 +0530

    Create feeds.py

---
+++ feeds.py
@@ -0,0 +1,13 @@
+# feeds.py
+FEEDS = [
+    "https://indianexpress.com/feed/",
+    "https://www.thehindu.com/news/national/feeder/default.rss",
+    "https://feeds.feedburner.com/ndtvnews-top-stories?format=xml",
+    "https://www.hindustantimes.com/feeds/rss/india-news/rssfeed.xml",
+    "https://www.livemint.com/rss/news",
+    "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+    "https://www.deccanherald.com/rss.xml",
+    "https://www.indiatoday.in/rss/1206578",
+    "https://www.news18.com/rss/india.xml",
+    "https://economictimes.indiatimes.com/feeds/newsdefault.cms",
+]
